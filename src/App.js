c6bf71140b18ae7ce6b7fb8f38508ac660ed6e93
mattostraszewski <mrostraszewski@gmail.com>
2020-05-13 13:47:08 -0600

    Finished

--- src/App.js
+++ src/App.js
@@ -9,25 +9,59 @@ import TextContainer from './components/TextContainer';
 
 class App extends Component {
   // constructor
+  constructor() {
+    super()
 
+    this.state = {
+      fontColor: 'black',
+      fontSize: 12,
+      fontFamily: 'monospace',
+      allowEdit: 'true'
+    }
+    this.updateColor = this.updateColor.bind(this)
+    this.updateSize = this.updateSize.bind(this)
+    this.updateFamily = this.updateFamily.bind(this)
+    this.updateEditStatus = this.updateEditStatus.bind(this)    //I DO NOT KNOW WHAT THIS IS DOING
+
+  }
   // updateColor
-
+  updateColor(val) {
+    this.setState({
+      fontColor: val
+    })
+  }
   // updateSize
-
+  updateSize(value) {
+    this.setState({
+      fontSize: value
+    })
+  }
   // updateFamily
-
+  updateFamily(value) {
+    this.setState({
+      fontFamily: value
+    })
+  }
   // updateEditStatus
+  updateEditStatus(value) {
+    this.setState({
+      allowEdit: value
+    })
+  }
+
 
   render() {
+    const { allowEdit, fontFamily, fontColor, fontSize } = this.state //what is this
     return (
       <div>
         <div className="headerBar">
-          {/* Render EditToggle */}
-          {/* Render ColorChanger */}
-          {/* Render SizeChanger */}
-          {/* Render FamilyChanger */}
+          <EditToggle update={this.updateEditStatus} />
+          <ColorChanger update={this.updateColor} allowEdit={allowEdit} />
+          <SizeChanger update={this.updateSize} allowEdit={allowEdit} />
+          <FamilyChanger update={this.updateFamily} allowEdit={allowEdit} />
+
         </div>
-        <div className="textArea">{/* Render TextContainer */}</div>
+        <div className="textArea">{<TextContainer fontSize={fontSize} fontColor={fontColor} fontFamily={fontFamily} />}</div>
       </div>
     );
   }
